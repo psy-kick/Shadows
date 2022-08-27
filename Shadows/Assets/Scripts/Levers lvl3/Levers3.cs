080fbee26cb61b5b91fe49acd58c2844e8620c7b
psy-kick <suhasadityasm@gmail.com>
2022-08-27 15:34:54 +0530

    Made Interactable for levers and switches

---
+++ Shadows/Assets/Scripts/Levers lvl3/Levers3.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levers3 : MonoBehaviour
+{
+    public GameObject TrapDoor;
+    bool canInteract;
+    public string LeversAnim;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canInteract)
+        {
+            GetAnimators();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Shadow")
+        {
+            canInteract = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canInteract = false;
+    }
+    void GetAnimators()
+    {
+        TrapDoor.GetComponent<Animator>().SetTrigger(LeversAnim);
+    }
+}
