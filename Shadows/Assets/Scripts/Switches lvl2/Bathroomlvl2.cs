4f9aaab7b9c43894c8818e466444b1d319904de1
psy-kick <suhasadityasm@gmail.com>
2022-08-27 22:32:04 +0530

    Level 2 done

---
+++ Shadows/Assets/Scripts/Switches lvl2/Bathroomlvl2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bathroomlvl2 : MonoBehaviour
+{
+    public GameObject BathroomSwitch;
+    bool canInteract;
+    Mover player;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canInteract)
+        {
+            player.p_Levers(BathroomSwitch);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player = collision.GetComponent<Mover>();
+        if (player == null)
+        {
+            canInteract = false;
+        }
+        if (collision.tag == "Player")
+        {
+            canInteract = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canInteract = false;
+    }
+}
